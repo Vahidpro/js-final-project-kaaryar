1137f31f0da849d039542175baf63dd27797c968
Vahidpro <ivahidp@gmail.com>
2023-11-13 17:21:42 +0330

    Add Todo items from server

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 todoContainer = document.querySelector(".todo-container");
 
+renderTask = (task) => {
+	todoContainer.innerHTML += `
+	<li class="todo-item rounded-4 px-2 m-2">
+                        <div class="d-flex align-items-center justify-content-between">
+                            <span class="">${task.title}</span>
+                            <div class="icons">
+                                <button class="btn"><img src="./assets/delete.svg" alt=""></button>
+                                <button class="btn"><img src="./assets/done.svg" alt=""></button>
+                            </div>
+                        </div>
+                    </li>
+	`;
+};
+
 const url = "http://localhost:3000/todos";
 
 const getTodosData = () => {
@@ -8,7 +22,10 @@ const getTodosData = () => {
 			return res.json();
 		})
 		.then((data) => {
-			console.log(data);
+			data.forEach((task) => {
+				renderTask(task);
+			});
+			// renderTask(data[0]);
 		});
 };
 
